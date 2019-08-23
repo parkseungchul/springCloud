5930ecfbdfd87df8bfc0aa254e640d17cd508d38
parkseungchul@gmail.com <ccie6198>
2019-08-23 16:15:06 +0900

    refresh 적용을 위한 어노테이션 추가

--- mas01/order/src/main/java/com/psc/cloud/order/config/ServerConfig.java
+++ mas01/order/src/main/java/com/psc/cloud/order/config/ServerConfig.java
@@ -1,24 +1,32 @@
 package com.psc.cloud.order.config;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 @Setter
-public class ServerConfig {
+@RefreshScope
+@JsonSerialize(as=ServerConfig.class)
+public class ServerConfig{
 
-    @Value("${spring.application.name}")
-    private String appName;
-
+    // 활성화 된 프로파일 이름
     @Value("${spring.profiles.active}")
     private String profilesActive;
 
+    // 바라봐야 하는 서비스(어플리케이션 이름 없는 경우 default)
+    @Value("${spring.application.name}")
+    private String appName;
+
+    // 컴피그 서버에서 가져오는 값
     @Value("${service.name}")
     private String serviceName;
 
+    // 컴피그 서버에서 가져오는 값
     @Value("${service.local}")
     private String serviceLocal;
 }
