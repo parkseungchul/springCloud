81cc47b91006267d0ae72869614b35d619290b00
parkseungchul@gmail.com <ccie6198>
2019-08-23 16:44:50 +0900

    컴피그 - 오더 - 워크 구조 생성

--- mas01/order/src/main/java/com/psc/cloud/order/controller/ComController.java
+++ mas01/order/src/main/java/com/psc/cloud/order/controller/ComController.java
@@ -5,9 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-    public class ComController {
-
+@RestController
+public class ComController {
 
     @Autowired
     private ServerConfig serverConfig;
---
+++ mas01/work/src/main/java/com/psc/cloud/work/WorkApplication.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkApplication.class, args);
+    }
+
+}
