99a775e689ae1f6f8d26b67c5ca8a780387a7745
simon <parkseungchul@gmail>
2019-09-01 13:16:21 +0900

    service gateway tracking filter 확인을 위한 interceptor 설정 추가

---
+++ mas01/work/src/main/java/com/psc/cloud/work/config/Interceptor.java
@@ -0,0 +1,31 @@
+package com.psc.cloud.work.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class Interceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String tmxCorrelationId = request.getHeader("tmx-correlation-id");
+        log.info("Interceptor > preHandle: tmxCorrelationId (from service gateway) [" + tmxCorrelationId + "]");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //log.info("Interceptor > postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) throws Exception {
+        //log.info("Interceptor > afterCompletion" );
+    }
+}
---
+++ mas01/work/src/main/java/com/psc/cloud/work/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.psc.cloud.work.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final String[] EXCLUDE_PATHS = {
+            ""
+    };
+
+    @Autowired
+    private Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .excludePathPatterns(EXCLUDE_PATHS)
+                .addPathPatterns("/api/**");
+
+
+    }
+}
