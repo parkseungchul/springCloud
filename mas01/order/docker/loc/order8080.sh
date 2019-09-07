7d363cb1017a51f945457562986dd166276742bb
parkseungchul@gmail.com <ccie6198>
2019-09-07 18:30:53 +0900

    쉘 실행 오류 수정

--- mas01/order/docker/loc/order8080.sh
+++ mas01/order/docker/loc/order8080.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-java -jar ../../target/order-0.1.jar --server.port=8080
+cd ./mas01/order/target
+java -jar order-0.1.jar --server.port=8080
--- mas01/order/docker/loc/order8090.sh
+++ mas01/order/docker/loc/order8090.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-java -jar ../../target/order-0.1.jar --server.port=8090
+cd ./mas01/order/target
+java -jar order-0.1.jar --server.port=8090
