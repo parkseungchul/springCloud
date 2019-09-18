394c0a145899dfee83a74840a4cb8d3171976aed
parkseungchul@gmail.com <ccie6198>
2019-09-18 10:46:42 +0900

    it is wrong code before. so it modified

--- mas01/protect2/src/main/java/com/psc/cloud/protect2/config/ResourceServerConfiguartion.java
+++ mas01/protect2/src/main/java/com/psc/cloud/protect2/config/ResourceServerConfiguartion.java
@@ -7,42 +7,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 @Configuration
 public class ResourceServerConfiguartion extends ResourceServerConfigurerAdapter {
-    private static final String[] EXCLUDE_PATHS = {
-            "/actuator"
-    };
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        //http.authorizeRequests().anyRequest().authenticated();
         http
                 .authorizeRequests()
-
-                .antMatchers(HttpMethod.GET, "/actuator")
-                .anonymous()
-                .antMatchers(HttpMethod.GET, "/actuator")
-                .permitAll()
-
-                .antMatchers(HttpMethod.GET, "/api")
-                .anonymous()
-                .antMatchers(HttpMethod.GET, "/api")
-                .permitAll()
-
-                .antMatchers(HttpMethod.GET, "/api/protect2")
-                .anonymous()
-                .antMatchers(HttpMethod.GET, "/api/protect2")
-                .permitAll()
-
-
                 .antMatchers(HttpMethod.GET, "/api/admin")
                 .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
-
+                .antMatchers(HttpMethod.GET, "/api/protect2/admin")
+                .hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/user")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/protect2/user")
                 .hasAnyRole("USER", "ADMIN")
                 .anyRequest()
                 .authenticated();
-
-
     }
 }
