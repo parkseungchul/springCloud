abf89594e1288910b1b2314c9d86ddb82df6e244
parkseungchul@gmail.com <ccie6198>
2019-09-18 09:32:05 +0900

    add security info for feign header

--- mas01/protect/src/main/java/com/psc/cloud/protect/clients/Protect2Client.java
+++ mas01/protect/src/main/java/com/psc/cloud/protect/clients/Protect2Client.java
@@ -1,11 +1,10 @@
 package com.psc.cloud.protect.clients;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "protect2")
+@FeignClient(value = "protect2", configuration = {Protect2ClientHeader.class})
 public interface Protect2Client {
 
     @RequestMapping(
@@ -13,4 +12,16 @@ public interface Protect2Client {
             value = "/api/protect2",
             consumes = "application/json")
     String getProtect2();
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/api/protect2/admin",
+            consumes = "application/json")
+    String getProtect2Admin();
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/api/protect2/user",
+            consumes = "application/json")
+    String getProtect2User();
 }
