5f98b15712753b8f351c0bb53a2e8d5d1f5453b2
parkseungchul@gmail.com <ccie6198>
2019-10-04 01:29:02 +0900

    소스 정리

---
+++ mas01A/product/src/main/java/com/psc/cloud/product/domain/Product.java
@@ -0,0 +1,4 @@
+package com.psc.cloud.product.domain;
+
+public class Product {
+}
---
+++ support/src/main/java/com/psc/cloud/support/SupportApplication.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportApplication.class, args);
+    }
+
+}
