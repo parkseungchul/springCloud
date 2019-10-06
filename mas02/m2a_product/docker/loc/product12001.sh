f5cb100aa45c91e48245182deefecb27b0419426
parkseungchul@gmail.com <ccie6198>
2019-10-06 13:13:55 +0900

    환경변수 > application.yml 우선순위 env.init

--- mas02/m2a_product/docker/loc/product12001.sh
+++ mas02/m2a_product/docker/loc/product12001.sh
@@ -1,2 +1,2 @@
-cd ./mas02/product/target
-java -jar product-0.1.jar --server.port=12001
+cd ./mas02/m2a_product/target
+java -jar product-0.1.jar --server.port=12001 --env.init=false
--- mas02/m2a_product/docker/loc/product12002.sh
+++ mas02/m2a_product/docker/loc/product12002.sh
@@ -1,2 +1,2 @@
 cd ./mas02/product/target
-java -jar product-0.1.jar --server.port=12002
+java -jar product-0.1.jar --server.port=12002 --env.init=false
